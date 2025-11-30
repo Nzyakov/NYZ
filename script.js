2c5e7ccd03a8baef813e917458993628eb79fa2c
Nzayakov <nazaryakovenko6@gmail.com>
2025-11-30 07:49:24 +0100

    при спробі зробити перемикання тем стилі злетіли повністю....

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
 let currentLang = localStorage.getItem('language') || 'cs';
+let currentTheme = localStorage.getItem('theme') || 'dark';
+
+const themeLink = document.getElementById('themeLink');
+
 function setLanguage(lang) {
     fetch(`Lang/${lang}.json`)
         .then(res => res.json())
         .then(data => {
             document.querySelectorAll('[data-lang]').forEach(el => {
                 const key = el.getAttribute('data-lang');
-                if (data[key]) {
-                    el.innerHTML = data[key];
-                }
+                if (data[key]) el.innerHTML = data[key];
             });
             currentLang = lang;
             localStorage.setItem('language', lang);
-            
         })
         .catch(err => console.error("Ошибка загрузки языка:", err));
 }
+
+
+function setTheme(theme) {
+    themeLink.href = theme === 'light' ? 'style_light.css' : 'style_dark.css';
+    currentTheme = theme;
+    localStorage.setItem('theme', theme);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+   
     setLanguage(currentLang);
-});
+    setTheme(currentTheme);
 
+   
+    const selected = document.querySelector('.select-selected');
+    const itemsContainer = document.querySelector('.select-items');
 
-const langSelect = document.getElementById('languageSelect');
+    if(selected && itemsContainer){
+        selected.addEventListener('click', () => {
+            itemsContainer.classList.toggle('select-hide');
+        });
 
-document.addEventListener('DOMContentLoaded', () => {
-    langSelect.value = localStorage.getItem('language') || 'cs';
-    setLanguage(langSelect.value);
+        itemsContainer.querySelectorAll('div').forEach(item => {
+            item.addEventListener('click', () => {
+                const lang = item.getAttribute('data-lang');
+                setLanguage(lang);
+                selected.innerHTML = item.innerHTML;
+                itemsContainer.classList.add('select-hide');
+            });
+        });
 
+        document.addEventListener('click', (e) => {
+            if(!e.target.closest('.custom-select')) itemsContainer.classList.add('select-hide');
+        });
+    }
 
-});
-
-langSelect.addEventListener('change', () => {
-    setLanguage(langSelect.value);
+    const themeSwitcher = document.getElementById('themeSwitcher');
+    if(themeSwitcher){
+        themeSwitcher.addEventListener('click', () => {
+            const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+            setTheme(newTheme);
+        });
+    }
 });
